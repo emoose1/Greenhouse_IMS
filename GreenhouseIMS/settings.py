d18685508032f2b9c645b1135694b26c9f0f1db8
EmmanuelMuse <Emmanuel.Muse@ghtechlabs.com>
2020-04-01 01:32:19 -0400

    whitenoise

--- GreenhouseIMS/settings.py
+++ GreenhouseIMS/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'GreenhouseIMS.urls'
@@ -128,6 +129,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CRISPY_TEMPLATE_PACK="bootstrap4"
 
