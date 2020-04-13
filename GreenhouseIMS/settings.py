cd3baa70e5d08bcd9607a8f372360d1ec0c63958
EmmanuelMuse <Emmanuel.Muse@ghtechlabs.com>
2020-04-13 17:02:44 -0400

    Update settings.py

--- GreenhouseIMS/settings.py
+++ GreenhouseIMS/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qgsd6jg+lo1wtlywz202xq(a69dh^+aq=^1h)&p6yh^n231+q*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['greenhouse-ims.herokuapp.com', '127.0.0.1']
 
